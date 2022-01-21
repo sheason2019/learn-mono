ab460e7c906a2c09a7552591cdb4871bf653bdde
sheason <811627877@qq.com>
2022-01-21 16:05:19 +0800

    tower-defend/feat: add hotreload

--- packages/tower-defend/main.js
+++ packages/tower-defend/main.js
@@ -39,5 +39,9 @@ app.on('window-all-closed', function () {
   if (process.platform !== 'darwin') app.quit()
 })
 
+try {
+  require('electron-reloader')(module);
+} catch (_) {}
+
 // In this file you can include the rest of your app's specific main process
 // code. You can also put them in separate files and require them here.
