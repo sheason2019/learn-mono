06fe90611a0f0a7fb84961d4d10bdd042350e938
sheason <811627877@qq.com>
2022-01-24 10:53:37 +0800

    feat: create electron-vite-solidjs project

--- packages/electron-vite-solidjs/main.js
+++ packages/electron-vite-solidjs/main.js
@@ -1,8 +1,12 @@
 // Modules to control application life and create native browser window
-const {app, BrowserWindow} = require('electron')
+const { app, BrowserWindow } = require('electron')
 const path = require('path')
 
-function createWindow () {
+function createWindow() {
+  let isDev = false;
+  process.argv.forEach(arg => {
+    if (arg === '--development') isDev = true;
+  })
   // Create the browser window.
   const mainWindow = new BrowserWindow({
     width: 800,
@@ -13,7 +17,12 @@ function createWindow () {
   })
 
   // and load the index.html of the app.
-  mainWindow.loadFile('index.html')
+  // mainWindow.loadFile('index.html')
+  if (isDev) {
+    mainWindow.loadURL('http://localhost:3000/');
+  } else {
+    mainWindow.loadFile(path.dirname(__filename) + '/index.html');
+  }
 
   // Open the DevTools.
   // mainWindow.webContents.openDevTools()
@@ -38,10 +47,6 @@ app.whenReady().then(() => {
 app.on('window-all-closed', function () {
   if (process.platform !== 'darwin') app.quit()
 })
-
-try {
-  require('electron-reloader')(module);
-} catch (_) {}
 
 // In this file you can include the rest of your app's specific main process
 // code. You can also put them in separate files and require them here.
